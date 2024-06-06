07b8d5c01aacff7129fceb3a47077b5c2670782d
Shohada Sharmin <37999338+sharminshohada@users.noreply.github.com>
2024-06-05 17:24:10 -0700

    Add files via upload
    This script downsizes all MP4 videos in the current directory to a target height and width, and saves the downscaled videos in .mp4 format in a designated directory. The script processes all videos together in a batch process.

---
+++ Python/downscale_videos.py
@@ -0,0 +1,69 @@
+"""
+Author: Shohada Sharmin
+Date: 2024-06-05
+Description: This script downsizes all MP4 videos in the current directory to a target height and width,
+             and saves the downscaled videos in .mp4 format in a designated directory.
+             The script processes all videos together in a batch process.
+"""
+
+# Function to downscale a video to the target height and width.
+import cv2
+import os
+from tqdm import tqdm
+
+def downscale_video(video_path, output_path, target_height, target_width):
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        print(f"Failed to open file: {video_path}")
+        return
+
+    frame_rate = cap.get(cv2.CAP_PROP_FPS)
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    if width == 0 or height == 0:
+        print(f"Invalid dimensions for file: {video_path}")
+        cap.release()
+        return
+
+    if height > width:
+        fract = target_height / height
+    else:
+        fract = target_width / width
+
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    out = cv2.VideoWriter(output_path, fourcc, frame_rate, (int(width * fract), int(height * fract)))
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        resized_frame = cv2.resize(frame, (int(width * fract), int(height * fract)), interpolation=cv2.INTER_CUBIC)
+        out.write(resized_frame)
+
+    cap.release()
+    out.release()
+
+# Main function to process all MP4 videos in the current directory, downscale them, and save them in a 'DownScaled_Videos' directory.
+def main():
+    target_height = 512
+    target_width = 512
+    current_dir = os.getcwd()
+    file_list = os.listdir(current_dir)
+
+    downscaled_dir = os.path.join(current_dir, "DownScaled_Videos")
+    if not os.path.exists(downscaled_dir):
+        os.mkdir(downscaled_dir)
+
+    for file_name in tqdm(file_list):
+        if file_name.endswith(".MP4"):
+            video_path = os.path.join(current_dir, file_name)
+            base_name = os.path.splitext(file_name)[0]  # Get the base name of the file
+            output_file_name = base_name + "_downscaled.mp4"  # Append '_downscaled' and change extension to .mp4
+            output_path = os.path.join(downscaled_dir, output_file_name)
+            downscale_video(video_path, output_path, target_height, target_width)
+
+if __name__ == "__main__":
+    main()
